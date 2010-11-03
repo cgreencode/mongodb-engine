0a042a2cb9881cb66c11a6166af59bbdab96144c
anproagile <anproagile@gmail.com>
2010-11-04 00:36:08 +0100

    Added multi-update tests

--- django_mongodb_engine/fields.py
+++ django_mongodb_engine/fields.py
@@ -94,8 +94,7 @@ class EmbeddedModelField(DictField):
             if not values:
                 return None
 
-            if "_id" in values:
-                values["id"] = values.pop("_id")
+            values["id"] = values.pop('_id', None)
 
             # In version 0.2, the layout of the serialized model instance changed.
             # Cleanup up old instances from keys that aren't used any more.
